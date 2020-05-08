dee6c34afe2336299b094611715a41d59887c8ad
EvanTheSmith <ProfessionalEmail@Zoho.com>
2020-05-08 16:49:34 -0500

    Deleted comments in scraper class

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,14 +2,7 @@ class LOTR::Scraper
 # This class is responsible for scraping my LOTR API, using my personal key
     def scrape
         the_key = ENV["LOTR_KEY"]
-        # char_url = "https://the-one-api.herokuapp.com/v1/character"
         mov_url = "https://the-one-api.herokuapp.com/v1/movie"
-        # test = HTTParty.get(char_url, :headers => {"Authorization" => "Bearer #{the_key}"})
         movies = HTTParty.get(mov_url, :headers => {"Authorization" => "Bearer #{the_key}"})
-
-        movies["docs"].each do |thing|
-            byebug
-        end
-
     end
 end
