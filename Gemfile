b8a1c593a6664bf6a7a0657ffc09c2c80c150f3b
EvanTheSmith <ProfessionalEmail@Zoho.com>
2020-05-08 01:00:08 -0500

    Scraper still isn't working >:(

--- Gemfile
+++ Gemfile
@@ -1,9 +1,5 @@
 source "https://rubygems.org"
 
-# Specify your gem's dependencies in lotr_api.gemspec
-#gemspec
-
-# gem "rake", "~> 12.0"
 gem 'pry'
 gem 'dotenv-rails'
 gem 'httparty'
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,7 +3,7 @@ class LOTR::Scraper
     def test
         the_key = ENV["LOTR_API_KEY"]
         char_url = "https://the-one-api.herokuapp.com/v1/character"
-        test = HTTParty.get("char_url",  headers: { "Authorization" => "Bearer #{the_key}"})
+        test = HTTParty.get("char_url",  headers: {"Authorization" => "Bearer #{the_key}"})
         
         byebug
     end
