35789352db4de1d8fa43493d879972e88eee130f
EvanTheSmith <ProfessionalEmail@Zoho.com>
2020-05-08 02:05:08 -0500

    The scraper is still not working :(

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,9 +1,9 @@
 class LOTR::Scraper
 # This class is responsible for scraping my LOTR API, using my personal key
-    def test
+    def scrape
         the_key = ENV["LOTR_API_KEY"]
         char_url = "https://the-one-api.herokuapp.com/v1/character"
-        test = HTTParty.get("char_url",  headers: {"Authorization" => "Bearer #{the_key}"})
+        test = HTTParty.get("char_url", :headers => {"Authorization" => "Bearer #{the_key}"})
         
         byebug
     end
