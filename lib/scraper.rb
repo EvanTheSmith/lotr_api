d85a6bf798a9affe339a8d00cf365f4d901739b6
EvanTheSmith <ProfessionalEmail@Zoho.com>
2020-05-08 03:07:44 -0500

    I typed 'git add .' by mistake

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,7 @@ class LOTR::Scraper
         the_key = ENV["LOTR_KEY"]
         char_url = "https://the-one-api.herokuapp.com/v1/character"
         test = HTTParty.get(char_url, :headers => {"Authorization" => "Bearer #{the_key}"})
-        
+        test2 = JSON.parse(test)
         byebug
     end
 end
