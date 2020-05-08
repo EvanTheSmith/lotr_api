89a34bf01938be1f1a360666aa762ac0d9e6b678
EvanTheSmith <ProfessionalEmail@Zoho.com>
2020-05-08 18:26:58 -0500

    Scraper should be working and creating movie objects now

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,18 +6,18 @@ class LOTR::Scraper
         movies = HTTParty.get(mov_url, :headers => {"Authorization" => "Bearer #{the_key}"}) #movies
         skip_to_movies = 5
 
-        movies["docs"].each do |tester|
+        movies["docs"].each do |mov|
         if skip_to_movies > 0
             skip_to_movies -= 1
         else
-            # Create movie objects
             newmovie = LOTR::Movies.new
-            newmovie.name = 
-            newmovie.id_number = 
-            newmovie.budget = 
-            newmovie.box_rev = 
-            newmovie.award_nom = 
-            newmovie.award_win = 
+            newmovie.name = mov["name"].strip
+            newmovie.id_number = mov["_id"]
+            newmovie.budget = mov["budgetInMillions"]
+            newmovie.box_rev = mov["boxOfficeRevenueInMillions"]
+            newmovie.award_nom = mov["academyAwardNominations"]
+            newmovie.award_win = mov["academyAwardWins"]
+        end
         end
 
     end
