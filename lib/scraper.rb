4c2cc024fe77a33e242dcdf4591de4cd35232565
EvanTheSmith <ProfessionalEmail@Zoho.com>
2020-05-08 16:40:29 -0500

    changed scraper variables

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,15 +2,14 @@ class LOTR::Scraper
 # This class is responsible for scraping my LOTR API, using my personal key
     def scrape
         the_key = ENV["LOTR_KEY"]
-        char_url = "https://the-one-api.herokuapp.com/v1/character"
+        # char_url = "https://the-one-api.herokuapp.com/v1/character"
         mov_url = "https://the-one-api.herokuapp.com/v1/movie"
-        test = HTTParty.get(char_url, :headers => {"Authorization" => "Bearer #{the_key}"})
-        test2 = HTTParty.get(mov_url, :headers => {"Authorization" => "Bearer #{the_key}"})
+        # test = HTTParty.get(char_url, :headers => {"Authorization" => "Bearer #{the_key}"})
+        movies = HTTParty.get(mov_url, :headers => {"Authorization" => "Bearer #{the_key}"})
 
-        test2["docs"].each do |thing|
+        movies["docs"].each do |thing|
             byebug
         end
 
-        byebug
     end
 end
