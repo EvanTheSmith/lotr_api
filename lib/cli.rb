9d936a3961765d31ca3e5545159367ad0a6a8c31
EvanTheSmith <ProfessionalEmail@Zoho.com>
2020-05-08 20:15:25 -0500

    The CLI is working perfectly. Just need to add the parameters now

--- lib/cli.rb
+++ lib/cli.rb
@@ -4,49 +4,67 @@ attr_reader :movies
     def begin
         input = nil
         LOTR::Scraper.new.scrape
-        @movies = LOTR::Movies.all
+        @movies = reorder_movies(LOTR::Movies.all)
+        puts " "
         puts "Welcome to my Lord of the Rings movie program."
         movie_list
         prompt
         while input != "exit"
             input = gets.strip
             if input == "1"
-                puts "Show information for Fellowship"
                 puts " "
+                puts "Show information for Fellowship"
+                sleep 1
                 prompt
             elsif input == "2"
-                puts "Show information for Two Towers"
                 puts " "
+                puts "Show information for Two Towers"
+                sleep 1
                 prompt
             elsif input == "3"
+                puts " "
                 puts "Show information for Return of the King"
-                puts " "
+                sleep 1
                 prompt
-            elsif input == "list"
-                puts " "
+            elsif input.downcase == "list"
                 movie_list
                 prompt
-            elsif input == "exit"
-                #do nothing
+            elsif input.downcase == "exit"
+                break
             else
+                puts " "
                 puts "I do not understand. Please try again."
+                sleep 2
                 prompt
             end
         end
+        puts " "
         puts "The program has now terminated. Have a nice day."
     end
 
     def prompt
-        puts " "
-        puts "Enter the number corresponding with each row to get more information about each movie."
         puts " "
         puts "Type 'exit' to end the program or 'list' to display the original list again."
         puts " "
     end
 
     def movie_list
+        puts " "
+        puts "Enter the number corresponding with each row to get more information about each movie."
+        puts " "
         @movies.each.with_index(1) do |movie, ind|
             puts "#{ind}. #{movie.name}"
         end
     end
+
+    def reorder_movies(movie_list)
+        ordered_list = []
+        movie = 1
+        movie_list.each do |mov|
+            movie == 2 ? ordered_list.unshift(mov) : ordered_list << mov
+            movie += 1
+        end
+        ordered_list
+    end
+
 end
